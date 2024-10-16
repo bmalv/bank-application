915e413902aae2a2b91fe0deb9d110fcc8353235
bmalv <74278503+bmalv@users.noreply.github.com>
2024-10-16 13:35:54 -0500

    create debit card and pin for checkings

--- src/Checking.java
+++ src/Checking.java
@@ -7,11 +7,20 @@ public class Checking extends Account {
     public Checking(String name, String sSN, double initDeposit){
         super(name, sSN, initDeposit);
         accountNumber = "2" + accountNumber;
+        setDebitCard();
     }
 
     //list methods specific to the checking account
+    public void setDebitCard(){
+        debitCardNum = (int) (Math.random() * Math.pow(10, 12));
+        pinNumber = (int) (Math.random() * Math.pow(10, 4));
+    }
+
     public void showInfo() {
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Checking");
+        System.out.println("Your Checking Account features" +
+                "\n  Debit Card Number: " + debitCardNum +
+                "\n  Debit Card Pin: " + pinNumber
+        );
     }
 }
