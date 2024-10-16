f573d0170bd3d57aca69261dc4884d9eb3eebf45
bmalv <74278503+bmalv@users.noreply.github.com>
2024-10-16 11:59:17 -0500

    creating safety deposit box

--- src/Savings.java
+++ src/Savings.java
@@ -7,11 +7,20 @@ public class Savings extends Account {
     public Savings(String name, String sSN, double initDeposit){
         super(name, sSN, initDeposit);
         accountNumber = "1" + accountNumber;
+        setSafetyDepositBox();
     }
 
     //list methods specific to savings account
+    private void setSafetyDepositBox() {
+        depositBoxID = (int) (Math.random() * Math.pow(10,3));
+        depositBoxCode = (int) (Math.random() * Math.pow(10,4));
+    }
+
     public void showInfo() {
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Savings");
+        System.out.println("Your Savings Account features" +
+                "\n  Safety Deposit Box ID: " + depositBoxID +
+                "\n  Safety Deposit Box Key: " + depositBoxCode
+                );
     }
 }
