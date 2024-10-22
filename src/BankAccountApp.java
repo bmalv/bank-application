1c2000fec3239937263394dd5464499d57bc9130
bmalv <74278503+bmalv@users.noreply.github.com>
2024-10-22 10:48:20 -0500

    reading from csv file

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -1,14 +1,24 @@
+import java.util.List;
+
 public class BankAccountApp {
     public static void main(String[] args) {
-        Checking chkacc1 = new Checking("Tom Wilson", "321456879", 1500);
-
-        Savings savacc1 = new Savings("Rich Lowe", "123456789", 2500);
-
-
-        savacc1.showInfo();
-        System.out.println("*****************");
-        chkacc1.showInfo();
+//        Checking chkacc1 = new Checking("Tom Wilson", "321456879", 1500);
+//
+//        Savings savacc1 = new Savings("Rich Lowe", "123456789", 2500);
+//
+//
+//        savacc1.showInfo();
+//        System.out.println("*****************");
+//        chkacc1.showInfo();
 
         //Read a CSV File then create new accounts based on that data
+        List<String[]> newAccountHolders = util.CSV.read("NewBankAccounts.csv");
+        for(String[] accountHolder: newAccountHolders){
+            System.out.println("NEW ACCOUNT");
+            System.out.println(accountHolder[0]);
+            System.out.println(accountHolder[1]);
+            System.out.println(accountHolder[2]);
+            System.out.println(accountHolder[3]);
+        }
     }
 }
